15d34e0711e4bf603662d3409983dc8431eeb13c
Jennifer Parra <jparra.biblio@gmail.com>
2018-12-05 13:05:40 -0300

    Subiendo funciones cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,45 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, str) => {
+    let result = '';
+
+    for (let i = 0; i < str.length; i++) {
+      let strToAscii = str.charCodeAt(i);
+
+      if (strToAscii >= 65 && strToAscii <= 90) {
+        result += String.fromCharCode((strToAscii - 65 + parseInt(offset)) % 26 + 65);
+      }
+      else if (strToAscii >= 97 && strToAscii <= 122) {
+        result += String.fromCharCode((strToAscii - 97 + parseInt(offset)) % 26 + 97);
+      }
+      else if (strToAscii >= 48 && strToAscii <= 57) {
+        result += String.fromCharCode((strToAscii - 48 + parseInt(offset)) % 10 + 48);
+      }
+      else {
+        result += String.fromCharCode(strToAscii);
+      }
+    }
+    return result;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (offset, str) => {
+    let result = '';
+
+    for (let i = 0; i < str.length; i++) {
+      let strToAscii = str.charCodeAt(i);
+
+      if (strToAscii >= 65 && strToAscii <= 90) {
+        result += String.fromCharCode((strToAscii - 65 - parseInt(offset)) % 26 + 65);
+      }
+      else if (strToAscii >= 97 && strToAscii <= 122) {
+        result += String.fromCharCode((strToAscii - 97 - parseInt(offset)) % 26 + 97);
+      }
+      else if (strToAscii >= 48 && strToAscii <= 57) {
+        result += String.fromCharCode((strToAscii - 48 - parseInt(offset)) % 10 + 48);
+      }
+      else {
+        result += String.fromCharCode(strToAscii);
+      }
+    }
+    return result;
   }
-};
+}
