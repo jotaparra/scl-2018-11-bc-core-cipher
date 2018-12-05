f917454dc937cddd66f9794df4d0b1645c6ef783
Jennifer Parra <jparra.biblio@gmail.com>
2018-12-05 13:07:29 -0300

    subiendo manejo del DOM

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-/* Acá va tu código */
+window.onload = () => {
+  const btncipher = document.getElementById('btn1');
+
+  btncipher.addEventListener('click', () => {
+  let str = document.getElementById('mensaje').value;
+  let offset = document.getElementById('numberOffset').value;
+  let result = window.cipher.encode(offset, str);
+  document.getElementById('nuevoMensaje').innerHTML = result;
+  }); 
+
+ const decipherBtn = document.getElementById('btn2');
+
+  decipherBtn.addEventListener('click', () => {
+  let str = document.getElementById('mensaje').value;
+  let offset = document.getElementById('numberOffset').value;
+  let result = window.cipher.decode(offset, str);
+  document.getElementById('nuevoMensaje').innerHTML = result;
+  }); 
+
+  const cleanBtn = document.getElementById('btn3')    //o podemos eliminar esta linea y colocar inmediatamente  btn3.addEventListener('click', () => {
+  
+  cleanBtn.addEventListener('click', () => {
+    document.getElementById('mensaje').value = '';
+    document.getElementById('nuevoMensaje').value = '';
+    document.getElementById('numberOffset').value = '';
+    });
+}
