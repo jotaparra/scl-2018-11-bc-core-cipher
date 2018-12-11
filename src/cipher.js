3938296c956ee49d89b4dce497f586dc1a694cd6
Jennifer Parra <jparra.biblio@gmail.com>
2018-12-11 10:08:26 -0300

    agregué condición para que transforme el offset negativo a positivo

--- src/cipher.js
+++ src/cipher.js
@@ -2,17 +2,23 @@ window.cipher = {
   encode: (str, offset) => {
     let result = '';
     offset = parseInt(offset);
+    if (offset < 0) { offset = offset * -1 }
 
     for (let i = 0; i < str.length; i++) {
       let strToAscii = str.charCodeAt(i);
 
-      if (strToAscii >= 65 && strToAscii <= 90) {
+      if (strToAscii >= 65 && strToAscii <= 90) {     //condición para minúsculas
         result += String.fromCharCode((strToAscii - 65 + offset) % 26 + 65);
+        /* if (offset < 0) {
+          result += String.fromCharCode((strToAscii - 65 + (offset * -1)) % 26 + 65)
+        }
+        else { result += String.fromCharCode((strToAscii - 65 + offset) % 26 + 65) }
+      } */
       }
-      else if (strToAscii >= 97 && strToAscii <= 122) {
+      else if (strToAscii >= 97 && strToAscii <= 122) {  //condición para mayúsculas
         result += String.fromCharCode((strToAscii - 97 + offset) % 26 + 97);
       }
-      else if (strToAscii >= 48 && strToAscii <= 57) {
+      else if (strToAscii >= 48 && strToAscii <= 57) {  //condición para números
         result += String.fromCharCode((strToAscii - 48 + offset) % 10 + 48);
       }
       else {
@@ -47,6 +53,9 @@ window.cipher = {
       else if (strToAscii >= 48 && strToAscii <= 57) {
         result += String.fromCharCode((strToAscii - 48 - offset) % 10 + 48);
       }
+      else if (strToAscii === 241) {  //condición para ñ
+        result += String.fromCharCode((strToAscii - 241 - offset) + 241);
+      }
       else {
         result += String.fromCharCode(strToAscii);
       }
@@ -54,3 +63,8 @@ window.cipher = {
     return result;
   }
 }
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ window.onload = () => {
   const btncipher = document.getElementById('btn1');
 
   btncipher.addEventListener('click', () => {
-  let str = document.getElementById('mensaje').value;
-  let offset = document.getElementById('numberOffset').value;
-  let result = window.cipher.encode(str, offset);
-  document.getElementById('nuevoMensaje').innerHTML = result;
-  }); 
+    let str = document.getElementById('mensaje').value;
+    let offset = document.getElementById('numberOffset').value;
+    let result = window.cipher.encode(str, offset);
+    document.getElementById('nuevoMensaje').innerHTML = result;
+  });
 
- const decipherBtn = document.getElementById('btn2');
+  const decipherBtn = document.getElementById('btn2');
 
   decipherBtn.addEventListener('click', () => {
-  let str = document.getElementById('mensaje').value;
-  let offset = document.getElementById('numberOffset').value;
-  let result = window.cipher.decode(str, offset);
-  document.getElementById('nuevoMensaje').innerHTML = result;
-  }); 
+    let str = document.getElementById('mensaje').value;
+    let offset = document.getElementById('numberOffset').value;
+    let result = window.cipher.decode(str, offset);
+    document.getElementById('nuevoMensaje').innerHTML = result;
+  });
 
   const cleanBtn = document.getElementById('btn3')    //o podemos eliminar esta linea y colocar inmediatamente  btn3.addEventListener('click', () => {
-  
+
   cleanBtn.addEventListener('click', () => {
     document.getElementById('mensaje').value = '';
     document.getElementById('nuevoMensaje').value = '';
     document.getElementById('numberOffset').value = '';
-    });
+  });
 }
